70885f3662aa9f6c256df68d9de0a97b3d56a66f
jcoggins <jcog306749@earthlink.net>
2020-10-18 23:29:19 -0400

    initial commit

---
+++ VueRouterBasics/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import EventList from "@/views/EventList.vue";
+import EventShow from "@/views/EventShow.vue";
+import EventCreate from "@/views/EventCreate.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "event-list",
+    component: EventList
+  },
+  {
+    path: "/event",
+    name: "event-show",
+    component: EventShow
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
